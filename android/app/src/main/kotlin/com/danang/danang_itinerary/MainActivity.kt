c95c8864938a62eea34153905ad52538e0997ec7
Kaivin22 <kaivin22@github.com>
2026-07-30 10:07:23 +0700

    chore: remove old flutter project files

--- android/app/src/main/kotlin/com/danang/danang_itinerary/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.danang.danang_itinerary
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/shared/widgets/organisms/organisms.dart
+++
@@ -1,6 +0,0 @@
-// Organisms barrel
-export 'bottom_nav_shell.dart';
-export 'category_filter_row.dart';
-export 'map_overlay.dart';
-export 'place_grid.dart';
-export 'search_bar_widget.dart';
